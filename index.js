fd9423af6766bc8e583247ff069961a1dde49df5
bhavini97 <bkumar221b@gmail.com>
2025-03-31 12:47:59 +0530

    logging

--- index.js
+++ index.js
@@ -9,11 +9,24 @@ const leaderboardRouter = require('./routes/leadershipRoutes')
 const forgotpasswordRouter = require('./routes/forgetPassword');
 const {syncDB} = require('./models/centralized');
 const premiumRoutes = require('./routes/premiumTable');
+require('dotenv').config()
+const morgan = require('morgan');
+const path = require('path');
+const fs = require('fs');
+
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static('public'));
+
+// write logging data in this file
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname,'access.log'),
+    {flags :'a'}
+)
+
+app.use(morgan('combined',{stream:accessLogStream}))
 
 app.use('/expense',expRouter);
 
@@ -28,7 +41,7 @@ app.use('/password',forgotpasswordRouter);
 app.use('/table', premiumRoutes); 
 
 syncDB().then(() => {
-    app.listen(3000, () => console.log('Server running on port 3000'));
+    app.listen(process.env.PORT, () => console.log('Server running on port 3000'));
 }).catch(err=>{
     console.error('error while starting server',err);
 });
